3aeea79fb5e48f6c97b92f7d704a5348defce687
Shant <prashantghimire79@gmail.com>
2021-04-10 13:14:24 +0545

    created models

--- classmate/classmate/settings.py
+++ classmate/classmate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notice.apps.NoticeConfig',
+    'group.apps.GroupConfig',
 ]
 
 MIDDLEWARE = [
---
+++ classmate/notice/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from group.models import Group
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    created_date = models.DateTimeField()
+    expiry_date = models.DateTimeField()
+    notice_type = models.CharField(max_length=10, choices=notice_choices)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
